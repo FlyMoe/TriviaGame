b88a319f51e2ae3214302a28dc9e9b07fbb88f85
FlyMoe <rrgarcia@gmail.com>
2016-04-29 19:36:15 -0400

    Added images, mp3s, and started the code

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+
+var questions = {
+	question1:{question:120, answer1: , answer2:, answer3: , answer4:},
+	question2:{question:120, answer1: , answer2:, answer3: , answer4:},
+	question3:{question:120, answer1: , answer2:, answer3: , answer4:},
+	question4:{question:120, answer1: , answer2:, answer3: , answer4:},
+	question5:{question:120, answer1: , answer2:, answer3: , answer4:},
+	question6:{question:120, answer1: , answer2:, answer3: , answer4:},	
+}
