ac374e42f04dbd87f3ff0f8f64f3ce28a0683636
FlyMoe <rrgarcia@gmail.com>
2016-04-29 19:37:07 -0400

    Added code and the trivia questions and answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,11 @@
 
 var questions = {
-	question1:{question:120, answer1: , answer2:, answer3: , answer4:},
-	question2:{question:120, answer1: , answer2:, answer3: , answer4:},
-	question3:{question:120, answer1: , answer2:, answer3: , answer4:},
-	question4:{question:120, answer1: , answer2:, answer3: , answer4:},
-	question5:{question:120, answer1: , answer2:, answer3: , answer4:},
-	question6:{question:120, answer1: , answer2:, answer3: , answer4:},	
+	question1:{question:"Who is the author of The Hobbit and the Lord of the Rings trilogy?", answer1:"Robert Heinlein", answer2:"J. R. R. Tolkien", answer3:"Isaac Asimov" , answer4:"Ray Bradbury"},
+	question2:{question:"Who was the tallest player ever to play in the NBA?", answer1:"Manute Bol" , answer2:"Shaq", answer3:"Shawn Bradley" , answer4:"Patrick Ewing"},
+	question3:{question:"New Orleans is known as the birthplace of what type of music?", answer1:"Indie Pop" , answer2:"New Age", answer3:"R&B / Soul" , answer4:"Jazz"},
+	question4:{question:"How many Super Bowl Rings did Troy Aikman win?", answer1:"1" , answer2:"2", answer3:"3" , answer4:"4"},
+	question5:{question:"Superman is a fictional superhero from what fictional planet?", answer1:"Kryptonian" , answer2:"Krypton", answer3:"Kryptonite" , answer4:"Krypt"},
+	question6:{question:"Who painted the Sistine Chapel?", answer1:"Leonardo da Vinci" , answer2:"Claude Monet", answer3:"Michelangelo" , answer4:"Rembrandt"},	
 }
+
+
